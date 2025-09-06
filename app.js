d7a35062c21f51052877495a36982b383c739c47
Jamylle <jamylle.geiza@gmail.com>
2025-09-05 22:40:32 -0300

    Adicionei a função sortear

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let nomeAmigos = [];
 
 function adicionarAmigo() {
@@ -12,5 +11,16 @@ function adicionarAmigo() {
     let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML += `<li>${nome.value}</li>`;
     nome.value = "";
+  }
+}
+
+function sortearAmigo(params) {
+  if (nomeAmigos.length == 0) {
+    return alert("Adicione amigos primeiro!");
+  } else {
+    let numeroSorteado = Math.floor(Math.random() * nomeAmigos.length);
+    let amigoSorteado = nomeAmigos[numeroSorteado];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>O amigo sorteado foi: ${amigoSorteado}</li>`;
   }
 }
