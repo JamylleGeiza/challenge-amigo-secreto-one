ba2cd0f1359477fcb05cc20b32c18123d0f4da02
Jamylle <jamylle.geiza@gmail.com>
2025-09-05 22:21:54 -0300

    Adicionei a função adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeAmigos = [];
+
+function adicionarAmigo() {
+  let nome = document.getElementById("amigo");
+  if (nome.value == "") {
+    alert("O campo não pode estar vazio");
+  } else if (nomeAmigos.includes(nome.value)) {
+    alert("Amigo já adicionado");
+  } else {
+    nomeAmigos.push(nome.value);
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML += `<li>${nome.value}</li>`;
+    nome.value = "";
+  }
+}
